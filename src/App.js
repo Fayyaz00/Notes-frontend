ab23b761217f94d97e55218be424ba116e4c88df
Fayyaz00 <fayyazsulman@gmail.com>
2022-07-18 20:07:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = (props) => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Catz</h1>
+      <Animal animal={"rat"} />
     </div>
-  );
+  )
 }
 
-export default App;
+const Animal = (props) => {
+  const { animal } = props
+
+  if (animal === "rat") {
+    return (
+      <div>
+        <h1>I'm a rat</h1>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>I'm a {animal}</h1>
+      </div>
+    )
+  }
+}
+
+export default App

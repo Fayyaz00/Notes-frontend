991c454edce8464575956366303e027b1f407450
Fayyaz00 <fayyazsulman@gmail.com>
2022-07-18 21:25:27 -0400

    part 2 - start

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
+import { useEffect } from 'react';
 import './App.css';
 
 const App = (props) => {
 
+  const { notes } = props
+
   return (
     <div className="App">
       <h1>Catz</h1>
-      <Animal animal={"rat"} />
+      <Animal animal={"cat"} />
+
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note} />)}
+      </ul>
+
     </div>
+  )
+}
+
+const Note = (props) => {
+  const { note } = props
+
+  console.log("note", note);
+
+  return (
+    <li>{note.content}</li>
   )
 }
 
 const Animal = (props) => {
   const { animal } = props
 
-  if (animal === "rat") {
-    return (
-      <div>
-        <h1>I'm a rat</h1>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <h1>I'm a {animal}</h1>
-      </div>
-    )
-  }
+  useEffect(() => {
+    console.log(props);
+  }, [props])
+
+
+  return (
+    <div>
+      <h1>I'm a {animal}</h1>
+    </div>
+  )
 }
 
 export default App
